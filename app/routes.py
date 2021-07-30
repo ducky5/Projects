adcaeeb690c342b8aa2b435f05cc8a50ce00363b
Duck <yassirelyakine@gmail.com>
2021-07-30 19:10:50 +0100

    added 'Logged in as user' in home.html and assumptions.html

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, db, login_manager, socketio, thread, thread_lock
+from app import app, db, login_manager, socketio
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_socketio import emit
