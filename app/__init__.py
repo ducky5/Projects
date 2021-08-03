df72c17bed1bfa3e7710a4157c1f3e6fcf929826
Duck <yassirelyakine@gmail.com>
2021-08-03 22:34:44 +0100

    added flask-compress for response compression

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_compress import Compress
 from threading import Lock
 
 app = Flask(__name__)
@@ -23,6 +24,9 @@ socketio = SocketIO(app, async_mode=async_mode)
 
 # login manager
 login_manager = LoginManager(app)
+
+# compress responses globally
+Compress(app)
 
 # supply location of login page
 login_manager.login_view = 'login_page'
